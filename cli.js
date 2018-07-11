884f015708872f9f303b8b3b66245743367b8295
Jon Meyers <jonathon.d.meyers@gmail.com>
2018-07-11 10:44:04 +1000

    ENHANCE: Add personalisation with name

--- cli.js
+++ cli.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-
 const [,, ...args] = process.argv;
 
 const randomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
-if (args[0] && args[0].toLowerCase() == '-jak') {
+const chalk = require('chalk');
+const options = require('./options');
+const rand = randomInt(options.length);
+
+let name = null;
+
+if (args[0]) {
+  name = args[0].toLowerCase();
+}
+
+if (name == 'jak') {
   console.log(chalk.red('sorry, I don\'t know how to do that :('));
+} else if (name) {
+  console.log(chalk.green(`hi ${name}, ${options[rand]}`));
 } else {
-  const options = require('./options');
-  const rand = randomInt(options.length);
   console.log(chalk.green(options[rand]));
 }
